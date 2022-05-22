db446eb838a6002819076f51c9c74f8493cd0220
Alex Contreras <alex.code29@gmail.com>
2022-05-21 23:06:31 -0500

    initialize about section with react componets

---
+++ src/componets/Nav/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+
+function categorySelected(name) {
+    console.log(`${name} clicked`)
+}
+
+
+function Nav() {
+    const categories = [
+        {
+            name: 'Commercial',
+            description: 'Photo of grocery store, food trucks, and other commercial projects',
+        },
+        {
+            name: 'Portraits', 
+            description: 'Portraits of people in my life'
+        },
+        {
+            name: 'Food', 
+            description: 'Delicious delicacies'
+        },
+        {
+            name: 'Landscape', 
+            description: 'Fiels, farmhouses, waterfalls, and the beauty of nature'
+        }
+    ]
+    return (
+        <header>
+            <h2>
+                <a href='/'>
+                    <span role="img" aria-label="camera">📸</span> Oh Snap!
+                </a>
+            </h2>
+            <nav>
+                <ul className="flex-row">
+                    <li className="mx-2">
+                        <a href="#about">
+                            About Me
+                        </a>
+                    </li>
+                    <li>
+                        <span>Contact</span>
+                    </li>
+                    {categories.map((category) => (
+                        <li className="mx-1" key={category.name}>                   
+                        <span onClick={() => categorySelected(category.name)}>
+                            {category.name}
+                        </span>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Nav;
